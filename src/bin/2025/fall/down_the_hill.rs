f7e95d78099a0976aaf480837575c0cded844729
XFajk <ertyperty24@gmail.com>
2025-09-28 01:49:30 +0200

    solved the second problem of the 2025 fall set

--- src/bin/2025/fall/down_the_hill.rs
+++ src/bin/2025/fall/down_the_hill.rs
@@ -1,74 +1,159 @@
-use std::io::{self, Read};
+use std::{collections::HashSet, io::stdin};
+
+enum MaxResult {
+    Same,
+    LeftGrater,
+    RightGrater,
+}
+
+fn special_max(l: i64, r: i64) -> MaxResult {
+    if l == r {
+        MaxResult::Same
+    } else if l > r {
+        MaxResult::LeftGrater
+    } else {
+        MaxResult::RightGrater
+    }
+}
+
+fn solve(
+    path: &Box<[char]>,
+    position: usize,
+    level: i64,
+    mut current_max: i64,
+    mut max_position_set: HashSet<usize>,
+) -> HashSet<usize> {
+    match special_max(level, current_max) {
+        MaxResult::Same => {
+            max_position_set.insert(position);
+        }
+        MaxResult::LeftGrater => {
+            max_position_set.clear();
+            max_position_set.insert(position);
+            current_max = level
+        }
+        MaxResult::RightGrater => {} // do nothing
+    }
+
+    if position >= path.len() {
+        return max_position_set;
+    }
+
+    match path[position] {
+        '+' => {
+            max_position_set = solve(path, position + 1, level + 1, current_max, max_position_set);
+        }
+        '-' => {
+            max_position_set = solve(path, position + 1, level - 1, current_max, max_position_set);
+        }
+        '?' => {
+            let max_index_set1 = solve(
+                path,
+                position + 1,
+                level + 1,
+                current_max,
+                max_position_set.clone(),
+            );
+            let max_index_set2 =
+                solve(path, position + 1, level - 1, current_max, max_position_set);
+
+            max_position_set = max_index_set1.union(&max_index_set2).copied().collect();
+        }
+        _ => panic!("Invalid character in path"),
+    }
+
+    max_position_set
+}
 
 fn main() {
-    // read input
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input).unwrap();
-    let mut it = input.split_whitespace();
-    let n: usize = it.next().unwrap().parse().unwrap();
-    let s: Vec<char> = it.next().unwrap().chars().collect();
+    let _ = stdin()
+        .read_line(&mut String::new())
+        .expect("Failed to read the first parameter");
 
-    // 1) prefix S with v(+)=+1, v(-)=-1, v(?)=-1
-    let mut s_val = vec![0i32; n + 1];
-    for i in 1..=n {
-        let v = match s[i - 1] {
-            '+' => 1,
-            '-' => -1,
-            '?' => -1,
-            _ => unreachable!(),
-        };
-        s_val[i] = s_val[i - 1] + v;
-    }
-    // need[i] = max_{t<=i} S[t]
-    let mut need = vec![0i32; n + 1];
-    let mut cur_max = 0i32;
-    for i in 1..=n {
-        cur_max = cur_max.max(s_val[i]);
-        need[i] = cur_max;
-    }
+    let mut path: String = String::new();
+    let _ = stdin()
+        .read_line(&mut path)
+        .expect("Failed to read the path(second parameter)");
 
-    // 2) bal and q, then maxH
-    let mut bal = vec![0i32; n + 1];
-    let mut q = vec![0i32; n + 1];
-    for i in 1..=n {
-        bal[i] = bal[i - 1]
-            + match s[i - 1] {
-                '+' => 1,
-                '-' => -1,
-                _ => 0,
-            };
-        q[i] = q[i - 1] + if s[i - 1] == '?' { 1 } else { 0 };
-    }
-    let max_h = |i: usize| bal[i] + q[i];
+    let path: Box<[char]> = path.trim().chars().collect::<Vec<char>>().into();
 
-    // 3) maxSuffixS
-    let mut max_suf = vec![i32::MIN; n + 2];
-    max_suf[n] = s_val[n];
-    for i in (0..n).rev() {
-        max_suf[i] = max_suf[i + 1].max(s_val[i]);
-    }
+    let starting_level: i64 = 0;
+    let starting_position: usize = 0;
 
-    // 4) collect answers
-    let mut ans = Vec::new();
-    for i in 0..=n {
-        let ok_prefix = need[i] <= max_h(i);
-        let ok_suffix = if i == n {
-            true
-        } else {
-            max_suf[i + 1] <= s_val[i]
-        };
-        let ok_peak = i == 0 || s[i - 1] != '-';
-        if ok_prefix && ok_suffix && ok_peak {
-            ans.push(i);
+    let mut max_position_set: HashSet<usize> = HashSet::new();
+    max_position_set.insert(starting_position);
+
+    match path.first() {
+        Some('+') => {
+            let mut max_position_set = solve(
+                &path,
+                starting_position + 1,
+                starting_level + 1,
+                0,
+                max_position_set,
+            )
+            .into_iter()
+            .collect::<Vec<usize>>();
+
+            max_position_set.sort_unstable();
+
+            println!("{}", max_position_set.len());
+            for p in max_position_set {
+                print!("{} ", p);
+            }
+            println!()
         }
-    }
+        Some('-') => {
+            let mut max_position_set = solve(
+                &path,
+                starting_position + 1,
+                starting_level - 1,
+                0,
+                max_position_set,
+            )
+            .into_iter()
+            .collect::<Vec<usize>>();
 
-    println!("{}", ans.len());
-    for (k, x) in ans.iter().enumerate() {
-        if k > 0 {
-            print!(" ");
+            max_position_set.sort_unstable();
+
+            println!("{}", max_position_set.len());
+            for (i, p) in max_position_set.iter().enumerate() {
+                if i != 0 {
+                    print!(" ");
+                }
+                print!("{}", p);
+            }
+            println!();
         }
-        print!("{}", x);
+        Some('?') => {
+            let max_position_set1 = solve(
+                &path,
+                starting_position + 1,
+                starting_level + 1,
+                0,
+                max_position_set.clone(),
+            );
+            let max_position_set2 = solve(
+                &path,
+                starting_position + 1,
+                starting_level - 1,
+                0,
+                max_position_set,
+            );
+
+            let mut max_position_set = max_position_set1
+                .union(&max_position_set2)
+                .copied()
+                .collect::<Vec<usize>>();
+
+            max_position_set.sort_unstable();
+
+            println!("{}", max_position_set.len());
+            for p in max_position_set {
+                print!("{} ", p);
+            }
+            println!();
+        }
+        _ => panic!("Invalid first character in path"),
     }
-    println!();
 }
