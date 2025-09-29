aa008b92ed670c0ab53fe0f8e4b4b013462e7d00
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2025-09-29 08:49:30 +0200

    converted the recursive solution to a heap stack solution

--- src/bin/2025/fall/down_the_hill.rs
+++ src/bin/2025/fall/down_the_hill.rs
@@ -1,5 +1,6 @@
+use std::any::Any;
+use std::rc::Rc;
 use std::{collections::HashSet, io::stdin};
-use std::any::Any
 
 /* HEAP STACK CODE */
 
@@ -9,7 +10,10 @@ enum HeapStackReturn {
 }
 
 trait HeapStackPlate {
-    fn call(&mut self, last_return: Option<Box<dyn Any>>) -> HeapStackReturn;
+    fn call(
+        self: Box<Self>,
+        last_return: Option<Box<dyn Any>>,
+    ) -> (HeapStackReturn, Box<dyn HeapStackPlate>);
 }
 
 fn run_stack(plate: Box<dyn HeapStackPlate>) -> Box<dyn Any> {
@@ -17,16 +21,16 @@ fn run_stack(plate: Box<dyn HeapStackPlate>) -> Box<dyn Any> {
 
     let mut last_return: Option<Box<dyn Any>> = None;
 
-    while let Some(top) = stack.last_mut() {
+    while let Some(top) = stack.pop() {
         match top.call(last_return.take()) {
-            HeapStackReturn::Call(new_plate) => {
+            (HeapStackReturn::Call(new_plate), new_self) => {
+                stack.push(new_self);
                 stack.push(new_plate);
 
                 last_return = None;
             }
-            HeapStackReturn::Return(val) => {
-                stack.pop();
 
+            (HeapStackReturn::Return(val), _) => {
                 last_return = Some(val);
             }
         }
@@ -37,20 +41,143 @@ fn run_stack(plate: Box<dyn HeapStackPlate>) -> Box<dyn Any> {
 
 /* HEAP STACK CODE */
 
-struct SolutionPlate<'a> {
-    path: &'a Box<[char]>,
+#[derive(Copy, Clone, Debug)]
+enum SolutionState {
+    Start,
+    AfterFirstBranch,
+    Done,
+}
+
+struct SolutionPlate {
+    path: Rc<[char]>,
     position: usize,
     level: i64,
     current_max: i64,
     max_position_set: HashSet<usize>,
+
+    state: SolutionState,
 }
 
-impl<'a> HeapStackPlate for SolutionPlate<'a> {
-    fn call(&mut self, last_return: Option<Box<dyn Any>>) -> HeapStackReturn {
+impl HeapStackPlate for SolutionPlate {
+    fn call(
+        mut self: Box<Self>,
+        last_return: Option<Box<dyn Any>>,
+    ) -> (HeapStackReturn, Box<dyn HeapStackPlate>) {
+        match self.state {
+            SolutionState::Start => {
+                match special_max(self.level, self.current_max) {
+                    MaxResult::Same => {
+                        self.max_position_set.insert(self.position);
+                    }
+                    MaxResult::LeftGrater => {
+                        self.max_position_set.clear();
+                        self.max_position_set.insert(self.position);
+                        self.current_max = self.level;
+                    }
+                    MaxResult::RightGrater => {} // do nothing
+                }
 
+                if self.position >= self.path.len() {
+                    let max_position_set = self.max_position_set;
+                    self.max_position_set = HashSet::new();
+
+                    return (HeapStackReturn::Return(Box::new(max_position_set)), self);
+                }
+
+                match self.path[self.position] {
+                    '+' => {
+                        self.state = SolutionState::Done;
+
+                        let max_position_set = self.max_position_set;
+                        self.max_position_set = HashSet::new();
+
+                        (
+                            HeapStackReturn::Call(Box::new(SolutionPlate {
+                                path: Rc::clone(&self.path),
+                                position: self.position + 1,
+                                level: self.level + 1,
+                                current_max: self.current_max,
+                                max_position_set,
+                                state: SolutionState::Start,
+                            })),
+                            self,
+                        )
+                    }
+                    '-' => {
+                        self.state = SolutionState::Done;
+
+                        let max_position_set = self.max_position_set;
+                        self.max_position_set = HashSet::new();
+
+                        (
+                            HeapStackReturn::Call(Box::new(SolutionPlate {
+                                path: Rc::clone(&self.path),
+                                position: self.position + 1,
+                                level: self.level - 1,
+                                current_max: self.current_max,
+                                max_position_set,
+                                state: SolutionState::Start,
+                            })),
+                            self,
+                        )
+                    }
+                    '?' => {
+                        self.state = SolutionState::AfterFirstBranch;
+
+                        let max_position_set = self.max_position_set.clone();
+
+                        (
+                            HeapStackReturn::Call(Box::new(SolutionPlate {
+                                path: Rc::clone(&self.path),
+                                position: self.position + 1,
+                                level: self.level + 1,
+                                current_max: self.current_max,
+                                max_position_set,
+                                state: SolutionState::Start,
+                            })),
+                            self,
+                        )
+                    }
+                    _ => panic!("Invalid character in path"),
+                }
+            }
+            SolutionState::AfterFirstBranch => {
+                self.state = SolutionState::Done;
+
+                let max_position_set = self.max_position_set;
+                self.max_position_set = *last_return
+                    .unwrap()
+                    .downcast::<HashSet<usize>>()
+                    .unwrap();
+
+                (
+                    HeapStackReturn::Call(Box::new(SolutionPlate {
+                        path: Rc::clone(&self.path),
+                        position: self.position + 1,
+                        level: self.level - 1,
+                        current_max: self.current_max,
+                        max_position_set,
+                        state: SolutionState::Start,
+                    })),
+                    self,
+                )
+            }
+            SolutionState::Done => {
+                let temp_set = *last_return
+                        .unwrap()
+                        .downcast::<HashSet<usize>>()
+                        .unwrap();
+                
+                let result_set: HashSet<usize> = self.max_position_set
+                    .union(&temp_set)
+                    .cloned() 
+                    .collect();
+
+                (HeapStackReturn::Return(Box::new(result_set)), self)
+            }
+        }
     }
 }
-
 enum MaxResult {
     Same,
     LeftGrater,
@@ -67,55 +194,6 @@ fn special_max(l: i64, r: i64) -> MaxResult {
     }
 }
 
-fn solve(
-    path: &Box<[char]>,
-    position: usize,
-    level: i64,
-    mut current_max: i64,
-    mut max_position_set: HashSet<usize>,
-) -> HashSet<usize> {
-    match special_max(level, current_max) {
-        MaxResult::Same => {
-            max_position_set.insert(position);
-        }
-        MaxResult::LeftGrater => {
-            max_position_set.clear();
-            max_position_set.insert(position);
-            current_max = level
-        }
-        MaxResult::RightGrater => {} // do nothing
-    }
-
-    if position >= path.len() {
-        return max_position_set;
-    }
-
-    match path[position] {
-        '+' => {
-            max_position_set = solve(path, position + 1, level + 1, current_max, max_position_set);
-        }
-        '-' => {
-            max_position_set = solve(path, position + 1, level - 1, current_max, max_position_set);
-        }
-        '?' => {
-            let max_index_set1 = solve(
-                path,
-                position + 1,
-                level + 1,
-                current_max,
-                max_position_set.clone(),
-            );
-            let max_index_set2 =
-                solve(path, position + 1, level - 1, current_max, max_position_set);
-
-            max_position_set = max_index_set1.union(&max_index_set2).copied().collect();
-        }
-        _ => panic!("Invalid character in path"),
-    }
-
-    max_position_set
-}
-
 fn main() {
     let _ = stdin()
         .read_line(&mut String::new())
@@ -126,85 +204,44 @@ fn main() {
         .read_line(&mut path)
         .expect("Failed to read the path(second parameter)");
 
-    let path: Box<[char]> = path.trim().chars().collect::<Vec<char>>().into();
+    let path: Rc<[char]> = path.trim().chars().collect::<Vec<char>>().into();
 
     let starting_level: i64 = 0;
     let starting_position: usize = 0;
 
-    let mut max_position_set: HashSet<usize> = HashSet::new();
-    max_position_set.insert(starting_position);
+    let max_position_set = run_stack(Box::new(SolutionPlate {
+        path: Rc::clone(&path),
+        position: starting_position,
+        level: starting_level,
+        current_max: starting_level,
+        max_position_set: HashSet::new(),
 
-    match path.first() {
-        Some('+') => {
-            let mut max_position_set = solve(
-                &path,
-                starting_position + 1,
-                starting_level + 1,
-                0,
-                max_position_set,
-            )
-            .into_iter()
-            .collect::<Vec<usize>>();
+        state: SolutionState::Start,
+    }));
 
-            max_position_set.sort_unstable();
+    // let mut max_position_set = solve(
+    //     &path,
+    //     starting_position,
+    //     starting_level,
+    //     starting_level,
+    //     HashSet::new(),
+    // )
+    // .into_iter()
+    // .collect::<Vec<usize>>();
 
-            println!("{}", max_position_set.len());
-            for p in max_position_set {
-                print!("{} ", p);
-            }
-            println!()
-        }
-        Some('-') => {
-            let mut max_position_set = solve(
-                &path,
-                starting_position + 1,
-                starting_level - 1,
-                0,
-                max_position_set,
-            )
-            .into_iter()
-            .collect::<Vec<usize>>();
+    // max_position_set.sort_unstable();
 
-            max_position_set.sort_unstable();
+    let mut max_position_set = max_position_set
+        .downcast::<HashSet<usize>>()
+        .unwrap()
+        .into_iter()
+        .collect::<Vec<usize>>();
 
-            println!("{}", max_position_set.len());
-            for (i, p) in max_position_set.iter().enumerate() {
-                if i != 0 {
-                    print!(" ");
-                }
-                print!("{}", p);
-            }
-            println!();
-        }
-        Some('?') => {
-            let max_position_set1 = solve(
-                &path,
-                starting_position + 1,
-                starting_level + 1,
-                0,
-                max_position_set.clone(),
-            );
-            let max_position_set2 = solve(
-                &path,
-                starting_position + 1,
-                starting_level - 1,
-                0,
-                max_position_set,
-            );
+    max_position_set.sort_unstable();
 
-            let mut max_position_set = max_position_set1
-                .union(&max_position_set2)
-                .copied()
-                .collect::<Vec<usize>>();
-
-            max_position_set.sort_unstable();
-
-            println!("{}", max_position_set.len());
-            for p in max_position_set {
-                print!("{} ", p);
-            }
-            println!();
-        }
-        _ => panic!("Invalid first character in path"),
+    println!("{}", max_position_set.len());
+    for p in max_position_set {
+        print!("{} ", p);
     }
+    println!();
 }
